db6d802a6066c15d591ce55a51a9331802ebfe8e
RAIMOVKHASAN <hrk.itnnes@gmail.ru>
2023-06-11 21:33:38 +0300

    Initial Commit

---
+++ fanhasan/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  fanhasan
+//
+//  Created by mac on 11.06.2023.
+//  Copyright © 2023 mac. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
